0de681d50ba288cec86a9748b659bcd98a6186a0
Autumn <autumncbullard@gmail.com>
2017-12-23 01:14:06 -0500

    Add a header and a footer

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,12 @@ export default class App extends Component {
 
   render() {
     return (
-      <div className="container-fluid"></div>
+      <div className="container-fluid text-center">
+        <header>
+          <h1>Inspirational Quotes</h1>
+        </header>
+        <footer>Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></footer>
+      </div>
     );
   }
 }
