a364c440058d8bbfbe047fccfba24c57f37b8e44
Autumn <autumncbullard@gmail.com>
2017-12-24 09:46:41 -0500

    Add a button that generates a new random quote and a button that tweets the current quote

--- src/components/app.js
+++ src/components/app.js
@@ -33,7 +33,14 @@ export default class App extends Component {
           <div className="well center-block">
             <span className="fa fa-quote-left"></span><q>&nbsp;{this.state.randomQuote.quote}&nbsp;</q><span className="fa fa-quote-right"></span>
             <div id="source">&mdash; {this.state.randomQuote.source}</div>
+            <div className="text-right">
+              <a className="btn" href={`https://twitter.com/intent/tweet?text="${this.state.randomQuote.quote}" — ${this.state.randomQuote.source}`} target="_blank" id="tweet"><span className="fa fa-twitter fa-lg"></span>&nbsp;Tweet</a>
+            </div>
           </div>
+          <button type="button" className="btn btn-lg" id="new-quote" onClick={() => this.newQuote()}>
+            <span className="fa fa-repeat fa-2x"></span>
+            <span className="sr-only">load new quote</span>
+          </button>
         </main>
         <footer>Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></footer>
       </div>
