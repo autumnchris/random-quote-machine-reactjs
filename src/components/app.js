4477c537e28876a69c546047131c14670936befc
Autumn <autumncbullard@gmail.com>
2017-12-23 21:38:54 -0500

    Use axios get the JSON array of quotes

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('.././src/db/quotes.min.json').then((data) => {
+      this.setState({ quotes: data.data });
+    });
+  }
 
   render() {
     return (
